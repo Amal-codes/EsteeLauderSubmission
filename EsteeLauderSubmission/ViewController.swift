fcc4fa51b4b169baf5c10fab41a87c6926f1cf08
Amal <amalamohamed.2024@gmail.com>
2022-08-19 08:21:35 -0400

    I completed the Estee Lauder challenge!

--- EsteeLauderSubmission/ViewController.swift
+++ EsteeLauderSubmission/ViewController.swift
@@ -13,7 +13,54 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
 
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        let resultsController = storyboard?.instantiateViewController(withIdentifier: "results") as! ResultsViewController
+        resultsController.modalPresentationStyle = .pageSheet
+        resultsController.modalTransitionStyle = .coverVertical
+        present(resultsController, animated: true, completion: nil)
+    }
+}
+
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+
+
+class CelebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func loadDetails(_ sender: Any) {
+    }
+    
+
+    @IBAction func toQuiz(_ sender: Any) {
+        let quizController = storyboard?.instantiateViewController(withIdentifier: "quiz") as! QuizViewController
+        quizController.modalPresentationStyle = .pageSheet
+        quizController.modalTransitionStyle = .coverVertical
+        present(quizController, animated: true, completion: nil)
+    }
 }
 
